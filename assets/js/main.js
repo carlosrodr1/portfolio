06f581c780d9110b44dc22d2274e74c94b5ed4fb
Carlos Rodrigues <62635666+carlosrodr1@users.noreply.github.com>
2022-05-04 20:43:12 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){   
+      
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 75 * i)
+
+  });
+}
+
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
